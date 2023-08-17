3e24a44feb4cc1e7a915dd339662ed35dcb6a969
nino mal <nino_mrt@hotmail.com>
2023-08-16 21:12:49 -0300

    Subtotal & Total methods

--- src/main/java/ninomal/comp/corse/entities/Order.java
+++ src/main/java/ninomal/comp/corse/entities/Order.java
@@ -97,6 +97,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem item : items) {
+			sum += item.getSubtotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
