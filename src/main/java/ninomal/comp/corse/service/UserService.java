2e4426ab3ad52728933e87fc8dee7bba14a102cb
nino mal <nino_mrt@hotmail.com>
2023-08-18 17:01:53 -0300

    Upate exception

--- src/main/java/ninomal/comp/corse/service/UserService.java
+++ src/main/java/ninomal/comp/corse/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
 import ninomal.comp.corse.entities.User;
 import ninomal.comp.corse.repositories.UserRepository;
 import ninomal.comp.corse.service.exception.DatabaseException;
@@ -43,9 +44,13 @@ public class UserService{
 	}
 	
 	public User update(Long id, User obj){
-		User entity = repository.getReferenceById(id);
-		updateData(entity ,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity ,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
