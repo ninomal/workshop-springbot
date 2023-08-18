c70dc2c77d24dc358ec6cdb25b57f8128ea5bf82
nino mal <nino_mrt@hotmail.com>
2023-08-17 21:09:40 -0300

    Exception handling - delete

--- src/main/java/ninomal/comp/corse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/ninomal/comp/corse/resource/exception/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import ninomal.comp.corse.service.exception.DatabaseException;
 import ninomal.comp.corse.service.exception.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError standError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(standError);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseNotFound(DatabaseException e, HttpServletRequest request){
+		String error = "Data base not found";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError standError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(standError);
 	}
---
+++ src/main/java/ninomal/comp/corse/service/exception/DatabaseException.java
@@ -0,0 +1,10 @@
+package ninomal.comp.corse.service.exception;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
